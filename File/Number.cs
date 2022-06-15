81da393cd64023cb3ae560885f0784f4a75ee73a
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-15 17:00:16 -0300

    Add project files.

---
+++ File/Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File
+{
+    public class Number
+    {
+        using (StreamWriter file = new StreamWriter(@"C:\users\steph\OneDrive\Documents\Text.txt", true))
+        {
+            file.WriteLine(Number);
+        }
+        
+        
+    }
+}
---
+++ File/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "test";
+            File.WriteAllText(@"C\:users\steph\OneDrive\Documents\Text.txt", text);
+            
+            Console.WriteLine("Please enter a number: ");
+        }
+    }
+}
